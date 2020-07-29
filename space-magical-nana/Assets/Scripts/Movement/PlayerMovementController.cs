f554f1c1be7c550cb0cf7933707280ac592e66a8
Luis Diaz <ldiazn98@gmail.com>
2020-07-29 03:41:00 -0400

    Added sample scene to test

---
+++ space-magical-nana/Assets/Scripts/Movement/PlayerMovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Future player movement controller component, for now is
+/// just a test script for the moving entity
+/// </summary>
+public class PlayerMovementController : MonoBehaviour
+{
+
+    public Camera cam;
+    public float toleranceRadius = 10;
+    private MovingEntity movingEntity;
+
+    private void Start()
+    {
+        movingEntity = GetComponent<MovingEntity>();
+    }
+
+    private void Update()
+    {
+        Vector2 input = cam.ScreenToWorldPoint( Input.mousePosition ) - 
+                        transform.position;
+
+        if ( Input.GetKey(KeyCode.Mouse0) && input.sqrMagnitude > toleranceRadius * toleranceRadius)
+            movingEntity.Move(input);
+
+        Debug.Log(input);
+    }
+}
