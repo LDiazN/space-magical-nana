1f2e93492e82f7f711172ecd13221b1fb43f83b0
Luis Diaz <ldiazn98@gmail.com>
2020-07-29 03:38:52 -0400

    Added movement entity class and sample controller class

--- space-magical-nana/Assets/Scripts/Movement/MovingEntity.cs
+++ space-magical-nana/Assets/Scripts/Movement/MovingEntity.cs
@@ -20,6 +20,14 @@ public class MovingEntity : MonoBehaviour
     /// </summary>
     public float acceleration = 10.0f;
 
+    /// <summary>
+    /// How fast the entity deccelerates
+    /// </summary>
+    public float drag = 5f;
+
+    // Current direction
+    private Vector2 direction = Vector2.zero;
+
     // Current entity velocity
     private Vector2 velocity = Vector2.zero;
 
@@ -30,6 +38,29 @@ public class MovingEntity : MonoBehaviour
     {
         // init rigidbody
         rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // We will accelerate to the current direction 
+        // if the direction is not zero, otherwise 
+        // the entity will slow down
+        // Store the current direction in case its changed during the update function
+        Vector2 direction = this.direction;
+        this.direction = Vector2.zero;
+
+        if (direction == Vector2.zero)
+        {
+            float speed = velocity.magnitude;
+            Vector2 movementDir = velocity * (1 / speed);
+
+            velocity = speed - drag < 0 ? Vector2.zero : velocity - movementDir * drag;
+        }
+        else
+            velocity += acceleration * direction;
+
+        //Clamp to max speed
+        velocity = Vector2.ClampMagnitude(velocity, maxSpeed);
     }
 
     private void FixedUpdate()
@@ -47,10 +78,6 @@ public class MovingEntity : MonoBehaviour
     /// Direction to move at. The vector is normalized, so 
     /// it have no effect to pass a direction with a significant speed
     /// </param>
-    public void Move(in Vector2 direction)
-    {
-        Vector2 dir = direction.normalized;
-        velocity += direction * acceleration;
-    }
+    public void Move(in Vector2 direction) => this.direction = direction.normalized;
     
 }
