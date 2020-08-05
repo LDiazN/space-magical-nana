ffc5443b5b9c0840e973b3cf7145fbb88810ca85
Luis Diaz <ldiazn98@gmail.com>
2020-08-04 22:06:33 -0400

    Added object pool and poolable object

--- space-magical-nana/Assets/Scripts/Utils/ObjectPool.cs
+++ space-magical-nana/Assets/Scripts/Utils/ObjectPool.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System; 
 
 /// <summary>
 /// Simple class to provide an interface to get and return 
@@ -27,11 +28,32 @@ public class ObjectPool : MonoBehaviour
 
     void Awake()
     {
+        // Check if the given gameobject is a valid one
+        var poolableComp = pooledObject?.GetComponent<Poolable>();
+
+        if (pooledObject == null)
+            throw new ArgumentNullException(
+                "The given pooled object is null, it should be set to an object " +
+                "with a Poolable component"
+                );
+        else if (poolableComp == null)
+            throw new ArgumentException(
+                "The provided pooled object does not have a poolable component"
+            );
+
         // Create & fill the object stack
         objects = new Stack<GameObject>(initialObjectCount);
+        
         for (int i = 0; i < initialObjectCount; i++)
         {
+            // Create the object
             var o = Instantiate(pooledObject);
+
+            // get its poolable and subscribe to its event
+            poolableComp = o.GetComponent<Poolable>();
+            poolableComp.SubscribeDispose( Dispose );
+
+            // Push the object into the stack and deactivate it
             objects.Push(o);
             o.SetActive(false);
         }
@@ -56,7 +78,7 @@ public class ObjectPool : MonoBehaviour
     /// Return a game object to the pool
     /// </summary>
     /// <param name="gameObject"> Object to return </param>
-    public void Dispose(GameObject gameObject)
+    private void Dispose(GameObject gameObject)
     {
         gameObject.SetActive(false);
         objects.Push(gameObject);
---
+++ space-magical-nana/Assets/Scripts/Utils/Poolable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+/// <summary>
+/// This class provides an easy way to ensure correctness and 
+/// decoupling with object pooling. Every poolable object will 
+/// have an automated setup to communicate with its 
+/// corresponding pool.
+/// </summary>
+public class Poolable : MonoBehaviour
+{
+    /// <summary>
+    /// This event is called when the object should be deleted
+    /// </summary>
+    event Action<GameObject> DisposePoolable;
+
+    /// <summary>
+    /// Subscribe the given method to the DisposeEvent
+    /// </summary>
+    /// <param name="f"> 
+    /// Action to take when the poolable is disposed. It should
+    /// receive a reference to the disposed object (aka this)
+    /// </param>
+    public void SubscribeDispose(Action<GameObject> f) => DisposePoolable += f;
+
+    /// <summary>
+    /// Desubscribe from the DisposeEvent
+    /// </summary>
+    /// <param name="f">Method to remove from the subscribers methods</param>
+    public void DeSubscribeDispose(Action<GameObject> f) => DisposePoolable -= f;
+
+    /// <summary>
+    /// Delete this object from the scene. If it has a a pool, 
+    /// use the pools dispose. Otherwise destroy the object itself.
+    /// </summary>
+    public void Dispose() => DisposePoolable(gameObject);
+    
+}
