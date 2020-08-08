7d3180e652e2aa2aba667ba67449244f2ad3c68b
Luis Diaz <ldiazn98@gmail.com>
2020-08-08 13:06:08 -0400

    Added base skill class and started to code ui behavior

---
+++ space-magical-nana/Assets/Scripts/Skills/SkillMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new skillData", menuName = "Skill Data", order = 1)]
+public class SkillMetadata : ScriptableObject
+{
+    [SerializeField]
+    private string description;
+
+    [SerializeField]
+    private string skillName;
+
+    [SerializeField]
+    private Sprite icon;
+
+}
