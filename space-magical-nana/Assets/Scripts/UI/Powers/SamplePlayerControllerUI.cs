84563cdce66633c285a32bb97b54cbf85ed9c3ce
Luis Diaz <ldiazn98@gmail.com>
2020-08-08 13:06:59 -0400

    some meta

---
+++ space-magical-nana/Assets/Scripts/UI/Powers/SamplePlayerControllerUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamplePlayerControllerUI : MonoBehaviour
+{
+    public float speed = 10f;
+
+    public delegate void OnShowSkillsUI();
+
+    public event OnShowSkillsUI ShowSkillsUI;
+
+    public delegate void OnHideSkillsUI();
+
+    public event OnHideSkillsUI HideSkillsUI;
+
+
+    private void Update()
+    {
+        Vector2 input;
+
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            ShowSkillsUI();
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
+            HideSkillsUI();
+
+        transform.Translate(input * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ space-magical-nana/Assets/Scripts/UI/Powers/SkillsUIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillsUIController : MonoBehaviour
+{
+    public SamplePlayerControllerUI playerController; //@TODO we have to change this for the actual controller
+
+
+    private void Start()
+    {
+        playerController.ShowSkillsUI += OnShowSkillUI;
+        playerController.HideSkillsUI += OnHideSkillUI;
+    }
+
+    public void OnShowSkillUI() { }
+    public void OnHideSkillUI() { }
+}
