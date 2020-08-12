100ac6e8630f471a90c6d539cc5a395359e34b80
xdgorgola <15-11264@usb.ve>
2020-08-12 00:36:43 -0400

    Max and MinMax PropertyAttributes

---
+++ space-magical-nana/Assets/Utils/CustomAttributes/MaxAttribute/Editor/MinMaxDrawer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(MaxAttribute))]
+public class MaxDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //base.OnGUI(position, property, label);
+        EditorGUI.BeginProperty(position, label, property);
+
+        MaxAttribute max = attribute as MaxAttribute;
+
+        EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+        EditorGUI.indentLevel = 0;
+        EditorGUI.PropertyField(position, property);
+
+        switch (property.propertyType)
+        {
+            case SerializedPropertyType.Integer:
+                property.intValue = (property.intValue < max.Max ? property.intValue : (int)max.Max);
+                break;
+            case SerializedPropertyType.Float:
+                property.floatValue = (property.floatValue < max.Max ? property.floatValue : max.Max);
+                break;
+        }
+
+        EditorGUI.EndProperty();
+    }
+}
+
+[CustomPropertyDrawer(typeof(MinMaxAttribute))]
+public class MinMaxDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //base.OnGUI(position, property, label);
+        EditorGUI.BeginProperty(position, label, property);
+
+        MinMaxAttribute minMax = attribute as MinMaxAttribute;
+
+        EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+        EditorGUI.indentLevel = 0;
+        EditorGUI.PropertyField(position, property);
+
+        switch (property.propertyType)
+        {
+            case SerializedPropertyType.Integer:
+                property.intValue = (property.intValue > minMax.Min ? property.intValue : (int)minMax.Min);
+                property.intValue = (property.intValue < minMax.Max ? property.intValue : (int)minMax.Max);
+                break;
+            case SerializedPropertyType.Float:
+                property.floatValue = (property.floatValue > minMax.Min ? property.floatValue : minMax.Min);
+                property.floatValue = (property.floatValue < minMax.Max ? property.floatValue : minMax.Max);
+                break;
+        }
+
+        EditorGUI.EndProperty();
+    }
+}
---
+++ space-magical-nana/Assets/Utils/CustomAttributes/MaxAttribute/MaxAttribute.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Property | System.AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+public sealed class MaxAttribute : PropertyAttribute
+{
+    public readonly float Max;
+
+    public MaxAttribute(float max)
+    {
+        this.Max = max;
+    }
+}
+
+[System.AttributeUsage(System.AttributeTargets.Property | System.AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+public sealed class MinMaxAttribute : PropertyAttribute
+{
+    public readonly float Min, Max;
+
+    public MinMaxAttribute(float min, float max)
+    {
+        if (min > max)
+            throw new System.Exception("Invalid MinMax attribute values");
+
+        this.Min = min;
+        this.Max = max;
+    }
+}
