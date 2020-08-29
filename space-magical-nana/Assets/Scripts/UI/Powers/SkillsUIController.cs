3fd5d4dfb7a3ab3f67e4e48c47150937fbb1bd43
Luis Diaz <ldiazn98@gmail.com>
2020-08-29 11:31:15 -0400

    added code for the responsive UI behavior

--- space-magical-nana/Assets/Scripts/UI/Powers/SkillsUIController.cs
+++ space-magical-nana/Assets/Scripts/UI/Powers/SkillsUIController.cs
@@ -58,6 +58,25 @@ public class SkillsUIController : MonoBehaviour
     private float _animationTime = 0.02f;
 
     /// <summary>
+    /// Player camera
+    /// </summary>
+    [SerializeField]
+    private Camera _cam;
+
+    /// <summary>
+    /// rect extents used for the player to check margins
+    /// </summary>
+    [SerializeField]
+    private Vector2 _playerRectsSize;
+
+    /// <summary>
+    /// How far from the player we should cast the rects
+    /// </summary>
+    [SerializeField]
+    [Min(0f)]
+    private float _playerRectsRadius;
+
+    /// <summary>
     /// Just a handy way to store all the data for a single UI
     /// skill button together
     /// </summary>
@@ -78,6 +97,44 @@ public class SkillsUIController : MonoBehaviour
     /// </summary>
     private List<SkillBtnData> _skillButtons;
 
+
+    /// <summary>
+    /// A rect containing the world part that's entirely
+    /// contained by the screen
+    /// </summary>
+    private Rect _worldExtents;
+
+    /// <summary>
+    /// We check if the skills will be visible by checking
+    /// that all the surroinding rects are totally contained in 
+    /// the world extents rect
+    /// </summary>
+    private Rect[] _playerRects = new Rect[4];
+
+    private enum RectAt
+    {
+        Left  = 0,
+        Down  = 1,
+        Right = 2,
+        Top   = 3
+    }
+
+
+    private enum UIState
+    {
+        Top,
+        Bottom,
+        Left,
+        Right,
+        TopRight,
+        TopLeft,
+        BottomRight,
+        BottomLeft
+    }
+
+    private UIState _currentState = UIState.Bottom;
+
+    // -- In game Execution ------------------
     private void Start()
     {
         // clamp the offset degrees so it's contained in 360 degrees
@@ -120,8 +177,40 @@ public class SkillsUIController : MonoBehaviour
 
         // Set the actual position for each button
         ComputePositions();     //Set each button data its corresponding position
-        //UpdateBtnPositions();   //Set the button game object to that position
 
+        // Get the world coordinates based on the camera and 
+        // create the worldExtents rect
+
+        var min = new Vector2(0, 0);
+        var max = new Vector2(Screen.width, Screen.height);
+        
+        min = _cam.ScreenToWorldPoint(min);
+        max = _cam.ScreenToWorldPoint(max);
+
+        _worldExtents = new Rect(min, max - min);
+
+        // Init the size for each detecting rect
+
+        for(int i = 0; i < _playerRects.Length; i ++)
+        {
+            var temp = _playerRects[i];
+            temp.size = _playerRectsSize;
+            _playerRects[i] = temp;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(_worldExtents.center, _worldExtents.size);
+        Gizmos.color = Color.yellow;
+
+        foreach (var r in _playerRects)
+        {
+            Gizmos.DrawWireCube(r.center, r.size);
+            Debug.Log("Rect: " + r);
+        }
+        
     }
 
     /// <summary>
@@ -129,7 +218,15 @@ public class SkillsUIController : MonoBehaviour
     /// </summary>
     private void OnShowSkillUI()
     {
+        // Compute the next position for the casting rects:
+        SetPlayerRectsPositions();
+        // In case there's an animation in progress
         StopAllCoroutines();
+        // Update where the ui should spawn
+        UpdateUIState();
+        // Compute the position relative to the parent where each skill should appear
+        ComputePositions();
+        // Animate the buttons 
         StartCoroutine(ButtonMotion(true));
     }
 
@@ -165,16 +262,60 @@ public class SkillsUIController : MonoBehaviour
         if ( nSkills == 0)
             return;
 
-        // Compute the initial position 
-        Vector2 from = Vector2.down;
-        var angle = _buttonsOffset * (Mathf.PI / 180);
+        // Compute angle offset based on the number of elements
+        // and the ui state
+        float angle;
+        switch (_currentState)
+        {
+            case UIState.Bottom:
+            case UIState.Top:
+            case UIState.Left:
+            case UIState.Right:
+                angle = Mathf.PI / nSkills;
+                break;
+            default:
+                angle = ( Mathf.PI / 2 ) / nSkills;
+                break;
+        }
+
+        // Compute the initial position from the current state
+        float startPos;
+        var pi = Mathf.PI;
+        switch (_currentState)
+        {
+            case UIState.TopRight:
+                startPos = pi / 4.0f;
+                break;
+            case UIState.Top:
+                startPos = pi / 2.0f;
+                break;
+            case UIState.TopLeft:
+                startPos = 3.0f * pi / 4.0f;
+                break;
+            case UIState.Left:
+                startPos = pi;
+                break;
+            case UIState.BottomLeft:
+                startPos = 5.0f * pi / 4.0f;
+                break;
+            case UIState.Bottom:
+                startPos = 3.0f * pi / 2.0f;
+                break;
+            case UIState.BottomRight:
+                startPos = 7.0f * pi / 4.0f;
+                break;
+            default:
+                startPos = 0.0f;
+                break;
+        }
+
         float init;
         if (nSkills % 2 == 0)
-            init =  3f * Mathf.PI / 2f 
+            init =  startPos 
                     - (nSkills - 1) / 2 * angle 
                     - angle / 2 ;
         else
-            init =  3f * Mathf.PI / 2f 
+            init =  startPos 
                     - nSkills / 2 * angle;
 
 
@@ -272,4 +413,80 @@ public class SkillsUIController : MonoBehaviour
 
         if (!isIn) HideSkills();
     }
+
+    // ---- Functions to position rects ---- //
+
+    /// <summary>
+    /// Whenever the player moves, the rects position have to be updates.
+    /// This function will update such positions
+    /// </summary>
+    private void SetPlayerRectsPositions()
+    {
+        var currPos = (Vector2) transform.position;
+        var extents = _playerRects[0].size / 2;
+        extents.x *= -1;
+        extents.y *= -3/2;
+
+        _playerRects[0].position = currPos + Vector2.left  * _playerRectsRadius + extents;
+        _playerRects[1].position = currPos + Vector2.down  * _playerRectsRadius + extents;
+        _playerRects[2].position = currPos + Vector2.right * _playerRectsRadius + extents;
+        _playerRects[3].position = currPos + Vector2.up    * _playerRectsRadius + extents;
+    }
+
+    /// <summary>
+    /// Check if the container entirely contains the 
+    /// conained rect
+    /// </summary>
+    /// <param name="container"> Container Rect </param>
+    /// <param name="contained"> Contained Rect </param>
+    /// <returns></returns>
+    private bool Contains(Rect container, Rect contained)
+    {
+        var bigMin = container.min;
+        var bigMax = container.max;
+        var smallMin = contained.min;
+        var smallMax = contained.max;
+
+        return bigMin.x   <= smallMin.x && bigMin.y   <= smallMin.y &&
+               smallMax.x <= bigMax.x   && smallMax.y <= bigMax.y;
+    }
+
+    private void UpdateUIState()
+    {
+        Func<Rect, bool> InWorld = (r) => Contains(_worldExtents, r);
+
+        var left  = _playerRects[(int)RectAt.Left ];
+        var right = _playerRects[(int)RectAt.Right];
+        var top   = _playerRects[ (int)RectAt.Top ];
+        var down  = _playerRects[(int)RectAt.Down ];
+
+        if (InWorld(left) && InWorld(right) && InWorld(down) && InWorld(top))
+            _currentState = UIState.Bottom;
+        else if (!InWorld(left) && !InWorld(down))
+            _currentState = UIState.TopRight;
+        else if (!InWorld(left) && !InWorld(top))
+            _currentState = UIState.BottomRight;
+        else if (!InWorld(top) && !InWorld(right))
+            _currentState = UIState.BottomLeft;
+        else if (!InWorld(down) && !InWorld(right))
+            _currentState = UIState.TopLeft;
+        else if (!InWorld(down))
+            _currentState = UIState.Top;
+        else if (!InWorld(top))
+            _currentState = UIState.Bottom;
+        else if (!InWorld(left))
+            _currentState = UIState.Right;
+        else if (!InWorld(right))
+            _currentState = UIState.Left;
+        else
+            throw new InvalidProgramException(
+                "In Updating ui position state: " + 
+                "player should not be able to have more than 2 of her sides " +
+                "blocked by the world limits"
+                );
+
+    }
+
+    
+
 }
