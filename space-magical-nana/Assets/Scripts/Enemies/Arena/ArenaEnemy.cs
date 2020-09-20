cb5c2abd46b43b7cc9479b690aeaa7f0b6f12c8c
Luis Diaz <ldiazn98@gmail.com>
2020-09-19 22:46:04 -0400

    added enemy killed signal

---
+++ space-magical-nana/Assets/Scripts/Enemies/Arena/ArenaEnemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaEnemy : BaseEnemy
+{
+
+    /// <summary>
+    /// The centinel point is a point in the screen 
+    /// where the arena enemy will do its stuff. It's necessary
+    /// since in the arena, the enemies will mostly stay at a fixed 
+    /// position where they will stay until they die.
+    /// </summary>
+    [SerializeField]
+    private Vector2 _centinelPoint;
+
+    /// <summary>
+    /// Point in the world where this enemy will spawn.
+    /// </summary>
+    [SerializeField]
+    private Vector2 _spawnPoint;
+
+    public override void Spawn()
+    {
+        transform.position = _spawnPoint;
+        StartCoroutine(GoToCentinelPoint());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            _movingEntity.MovePosition(_centinelPoint);
+
+    }
+
+    private IEnumerator GoToCentinelPoint ()
+    {
+        while ((transform.position - (Vector3) _centinelPoint).sqrMagnitude > 0.1)
+        {
+            _movingEntity.MovePosition(_centinelPoint); // null reference
+            yield return new WaitForSecondsRealtime(Time.fixedDeltaTime);
+        }
+    }
+
+}
---
+++ space-magical-nana/Assets/Scripts/Enemies/BaseEnemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[RequireComponent(typeof(MovingEntity))]
+[RequireComponent(typeof(BaseHealthSystem))]
+public class BaseEnemy : MonoBehaviour
+{
+    /// <summary>
+    /// Component to perform movement for this enemy
+    /// </summary>
+    protected MovingEntity _movingEntity;
+
+    /// <summary>
+    /// Component to manage the health for this enemy
+    /// </summary>
+    protected BaseHealthSystem _healthSystem;
+
+    /// <summary>
+    /// Event called when this enemy gets killed
+    /// </summary>
+    event Action<BaseEnemy> EnemyKilled;
+    
+
+    private void Awake()
+    {
+        // Set up components
+        _movingEntity = GetComponent<MovingEntity>();
+        _healthSystem = GetComponent<BaseHealthSystem>();
+    }
+
+    /// <summary>
+    /// Listen to the EnemyKilled event
+    /// </summary>
+    /// <param name="eventHandler"></param>
+    public void SubscribeEnemyKilled(Action<BaseEnemy> eventHandler) => EnemyKilled += eventHandler;
+
+    /// <summary>
+    /// Stop listening to the EnemyKilled event
+    /// </summary>
+    /// <param name="eventHandler"></param>
+    public void DeSubscribeEnemyKilled(Action<BaseEnemy> eventHandler) => EnemyKilled -= eventHandler;
+
+    /// <summary>
+    /// Function to be called when a enemy enters to the scene
+    /// </summary>
+    public virtual void Spawn() { }
+
+    /// <summary>
+    /// Called when an enemy gets killed
+    /// </summary>
+    public virtual void Killed()
+    {
+        EnemyKilled(this);
+    }
+}
